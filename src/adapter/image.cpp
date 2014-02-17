033892e92b52bf9ab4647d6fda34b4aa4da48b9b
tot <tor16384@yandex.ru>
2014-02-17 22:26:19 +0200

    fixed libpng version reader. I believe works for usual pngs

--- src/adapter/image.cpp
+++ src/adapter/image.cpp
@@ -2,7 +2,10 @@
 //but for mac I decided to use native Cocoa API (NSImage based) see src/os/mac/adapter/image.mm
 #include <adapter/image.hpp>
 #include <cmn/log.hpp>
+#include <cmn/image.hpp>
+
 #include <png.h>
+#include <vector>
 #include <stdio.h>
 
 namespace adapter
@@ -10,7 +13,7 @@ namespace adapter
         
 cmn::image_pt image_create_from_png( char const * szImgPath )
 {
-        cmn::image_pt img;        
+        cmn::image_root_pt img( new cmn::image_root_t );        
         char unsigned header[8];    // 8 is the maximum size that can be checked
         
         png_structp png_ptr;        
@@ -19,6 +22,9 @@ cmn::image_pt image_create_from_png( char const * szImgPath )
         int width, height;
         png_byte color_type, bit_depth;
         int number_of_passes;
+        int pitch, y;        
+        uint8_t * row_ptr;                
+        std::vector<png_bytep> row_pointers;
 
         /* open file and test for it being a png */
         FILE *fp = fopen(szImgPath, "rb");
@@ -66,9 +72,20 @@ cmn::image_pt image_create_from_png( char const * szImgPath )
         color_type = png_get_color_type(png_ptr, info_ptr);
         bit_depth = png_get_bit_depth(png_ptr, info_ptr);
 
+        if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) 
+                png_set_expand_gray_1_2_4_to_8(png_ptr);
+        
+        if (bit_depth == 16)
+              png_set_scale_16(png_ptr);
+        
+        if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+                png_set_gray_to_rgb(png_ptr);
+        
+        if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY)
+                png_set_add_alpha(png_ptr, 0xff, PNG_FILLER_AFTER);
+        
         number_of_passes = png_set_interlace_handling(png_ptr);
         png_read_update_info(png_ptr, info_ptr);
-
 
         /* read file */
         if (setjmp(png_jmpbuf(png_ptr)))
@@ -77,19 +94,29 @@ cmn::image_pt image_create_from_png( char const * szImgPath )
                 goto unwind_03;
         }
 
-//         row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-//         for (y=0; y<height; y++)
-//                 row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
-
-//        png_read_image(png_ptr, row_pointers);
-
-        fclose(fp);
-        return img;
+        row_pointers.resize(height);
+        pitch =  width * 4; //4 - rgba
+        img->memory = std::shared_ptr<uint8_t>( new uint8_t[ height * pitch ] );
+        row_ptr = img->memory.get();        
+        for (y=0; y<height; ++y)
+        {
+                  row_pointers[y] = row_ptr;
+                  row_ptr += pitch;
+        }
+        png_read_image(png_ptr, &row_pointers[0]);
+               
+        img->header.width = width;
+        img->header.height = height;
+        img->header.pitch = pitch;
+        img->header.format = cmn::format_rgba;
+        img->header.flags = 0;
+        img->data.bytes = img->memory.get();
         
 unwind_03: png_destroy_info_struct(png_ptr, &info_ptr);        
 unwind_02: png_destroy_read_struct(&png_ptr, 0, 0);
 unwind_01: fclose(fp);
-unwind_00: return cmn::image_pt();    
+unwind_00: 
+        return img;
 }
 
 }
--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,11 +1,13 @@
 #include <core/frame_recognizer.hpp>
 #include <alg/watershed.hpp>
+#include <adapter/image.hpp>
 #include <stdio.h>
 
 
 int main()
 {
-        
+        char const * path = "/home/tot/a.png";
+        cmn::image_pt img = adapter::image_create_from_png( path );
         
         return 0;
 }
